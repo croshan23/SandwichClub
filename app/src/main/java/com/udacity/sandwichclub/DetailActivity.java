836bd0720d988f42fefefe5b0e649112798d6f33
chaudhro <roshan.chaudhary@ge.com>
2018-06-16 13:54:49 -0500

    Add code to populate UI in Details Activity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,38 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        //Setting origin data
+        TextView originLabelTV = (TextView) findViewById(R.id.origin_tv);
+        originLabelTV.setText(sandwich.getPlaceOfOrigin());
+
+        //Setting AKA
+        TextView alsoKnownAsTV = (TextView) findViewById(R.id.also_known_tv);
+        List<String> akaList = sandwich.getAlsoKnownAs();
+        String akaString = "";
+        for (String aka : akaList) {
+            if (akaString.isEmpty())
+                akaString += aka;
+            else
+                akaString += ", " + aka;
+        }
+        alsoKnownAsTV.setText(akaString);
+
+        //Setting Description
+        TextView description = (TextView) findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
+
+        //Setting Ingredients
+        TextView ingredients = (TextView) findViewById(R.id.ingredients_tv);
+        List<String> ingredientsList = sandwich.getIngredients();
+        String ingredientsString = "";
+        for (String ingredient : ingredientsList) {
+            if (ingredientsString.isEmpty())
+                ingredientsString += ingredient;
+            else
+                ingredientsString += ", " + ingredient;
+        }
+        ingredients.setText(ingredientsString);
     }
 }
